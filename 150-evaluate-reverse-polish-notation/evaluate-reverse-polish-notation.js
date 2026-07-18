70b34ac2b58aed2c9a60db60395709f866dc81a2
PrashantParhad <44218430+PrashantParhad@users.noreply.github.com>
2026-07-18 22:55:55 +0530

    Time: 4 ms (81.48%) | Memory: 56.4 MB (86.44%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.js
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.js
@@ -3,23 +3,18 @@
  * @return {number}
  */
 var evalRPN = function(tokens) {
-    let exp;
     let stack = []
     for(let i=0;i<tokens.length;i++){
         if(isNaN(tokens[i])){
             let a = stack.pop()
             let b = stack.pop()
-            let curr;
-            // console.log(token[i],a,b)
-            if(tokens[i] === "+") curr = b+a 
-            else if(tokens[i] === "-") curr = b-a
-            else if(tokens[i] === "*") curr = b*a
-            else if(tokens[i] === "/") curr = Math.trunc(b/a)
-            console.log(curr)
-            stack.push(curr)
+            if(tokens[i] === "+") stack.push(b+a) 
+            else if(tokens[i] === "-")  stack.push(b-a)
+            else if(tokens[i] === "*")  stack.push(b*a)
+            else if(tokens[i] === "/") stack.push(Math.trunc(b/a))
         } else {
             stack.push(Number(tokens[i]))
         }
     }
-    return stack[0];
+    return stack.pop();
 };
